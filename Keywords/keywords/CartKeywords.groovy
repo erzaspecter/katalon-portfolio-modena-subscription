51fb12d9d6ea205984921604c9ffceafb2db8342
erza akbar <erza.akbar@modena.com>
2026-05-17 13:03:14 +0700

    add tc 03

--- Keywords/keywords/CartKeywords.groovy
+++ Keywords/keywords/CartKeywords.groovy
@@ -31,4 +31,56 @@ public class CartKeywords {
         WebUI.click(findTestObject('TC-01 Add Product To Cart/Page_Water Purifier Reverse Osmosis Tabletop  MODENA/button_Add Subscription'))
         WebUI.click(findTestObject('TC-03 View Cart and Verify Product/a_Cart_Icon'))
     }
+	
+	
+	
+	/**
+	 * Open browser and navigate to subscription page
+	 */
+	static void openSubscriptionPage() {
+		WebUI.openBrowser('https://www.modena.com/seamless-go-subscription/id_en')
+		WebUI.waitForPageLoad(30)
+	}
+	
+	/**
+	 * Click Our Products link
+	 */
+	static void clickOurProducts() {
+		WebUI.click(findTestObject('TC-01 Add Product To Cart/Page_MODENA Subscription  MODENA/a_Our Products'))
+	}
+	
+	/**
+	 * Select product by ID
+	 * @param productId product ID to click (e.g., 'RO 5110 TEWH')
+	 */
+	static void selectProduct(String productId) {
+		WebUI.click(findTestObject('TC-01 Add Product To Cart/Page_MODENA Subscription  MODENA/div_RO 5110 TEWH                           _54940f'))
+	}
+	
+	/**
+	 * Select subscription duration
+	 * @param years duration in years (2, 3, 5, 6, or 7)
+	 */
+	static void selectDuration(int years) {
+		WebUI.click(findTestObject('TC-01 Add Product To Cart/Page_Water Purifier Reverse Osmosis Tabletop  MODENA/div_3                                                        year'))
+	}
+	
+	/**
+	 * Click Add Subscription button
+	 */
+	static void clickAddSubscription() {
+		WebUI.click(findTestObject('TC-01 Add Product To Cart/Page_Water Purifier Reverse Osmosis Tabletop  MODENA/button_Add Subscription'))
+	}
+	
+	/**
+	 * Complete add product flow
+	 * @param productId product to add
+	 * @param duration duration in years
+	 */
+	static void addProduct(String productId, int duration) {
+		clickOurProducts()
+		selectProduct(productId)
+		selectDuration(duration)
+		clickAddSubscription()
+	}
 }
--- Scripts/TC-01 Add Product To Cart/Script1778933533006.groovy
+++ Scripts/TC-01 Add Product To Cart/Script1778933533006.groovy
@@ -1,33 +1,14 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import keywords.CartKeywords
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
 
-//1. Buka browser
-WebUI.openBrowser('https://www.modena.com/seamless-go-subscription/id_en')
+// 1. Buka browser
+CartKeywords.openSubscriptionPage()
 
-//WebUI.maximizeWindow()
-WebUI.waitForPageLoad(30)
+// 2. Add product ke cart
+CartKeywords.addProduct('RO 5110 TEWH', 3)
 
-WebUI.click(findTestObject('TC-01 Add Product To Cart/Page_MODENA Subscription  MODENA/a_Our Products'))
+// 3. Verifikasi (opsional, bisa ditambahkan)
+WebUI.comment('TC-01 berhasil: Product added to cart')
 
-WebUI.click(findTestObject('TC-01 Add Product To Cart/Page_MODENA Subscription  MODENA/div_RO 5110 TEWH                           _54940f'))
-
-WebUI.click(findTestObject('TC-01 Add Product To Cart/Page_Water Purifier Reverse Osmosis Tabletop  MODENA/div_3                                                        year'))
-
-WebUI.click(findTestObject('TC-01 Add Product To Cart/Page_Water Purifier Reverse Osmosis Tabletop  MODENA/button_Add Subscription'))
-
+// 4. Tutup browser
+WebUI.closeBrowser()
